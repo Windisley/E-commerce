31b275c3b79c5633fbd8868110a50fa1fee84ecc
Windisley <windisleyl@gmail.com>
2024-10-27 01:10:59 -0300

    Renderizando cada item na sectionproduct

--- src/Components/SectionProduct.jsx
+++ src/Components/SectionProduct.jsx
@@ -1,10 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductsContext } from "../Context/Products";
 import { FaCartPlus } from "react-icons/fa";
 
+
+
+
 const SectionProduct = () => {
-    const {productinfo, setProductInfo} = useContext(ProductsContext)
-    console.log(productinfo)
+
+    
+    const { productinfo, setProductInfo } = useContext(ProductsContext)
+    
     return (
         <div className="
         max-w-full min-h-screen p-4 relative 
@@ -23,57 +28,66 @@ const SectionProduct = () => {
                     produtos
                 </h1>
 
+
+
                 <div className="
              w-full h-full grid place-items-center  grid-cols-gridpadrao 
              my-8 gap-2 justify-center
             ">
+
+                    {productinfo && productinfo.map((item) => (
+
                     <div className="
-                 w-full min-h-full p-4 flex items-center
-                 justify-center flex-col border-border1 
-                 rounded-lg gap-4
+             w-full min-h-full p-4 flex items-center
+             justify-center flex-col border-border1 
+             rounded-lg gap-4
+            " key={item.name}>
+                        <div className="
+                max-w-full h-full  bg-primarygray rounded-lg p-2 flex items-center
+                justify-center
                 ">
-                        <div className="
-                    max-w-full min-h-fufull  bg-primarygray rounded-lg p-2
-                    ">
-                            <img src={""} alt="#" className="
-                         w-full h-full object-center object-cover 
-                        "/>
+                            <img src={item.img} alt="#" className="
+                     w-full h-4/5 object-center object-cover
+                    "/>
                         </div>
-                       <div className="
-                       w-4/5 flex justify-around items-center
-                       ">
-                        <p className="
-                     uppercase font-bold text-base text-center
-                    ">
-                            Name
-                        </p>
-
-                     <div>
-                        <FaCartPlus className="
-                        text-lg hover:text-button cursor-pointer ease-in-out
-                        "/>
-                     </div>
-
-                       </div>
                         <div className="
-                      w-4/5 flex justify-around gap-2 h-full
-                    ">
+                   w-4/5 flex justify-around items-center
+                   ">
+                            <p className="
+                 uppercase font-bold text-base text-center
+                ">
+                                {item.name}
+                            </p>
+
+                            <div>
+                                <FaCartPlus className="
+                    text-lg hover:text-button cursor-pointer ease-in-out
+                    "/>
+                            </div>
+
+                        </div>
+                        <div className="
+                  w-4/5 flex justify-around gap-2 h-full
+                ">
                             <button className="
-                        max-w-full h-full bg-button px-8 py-1 rounded-lg
-                        text-base text-colorWhite hover:bg-colorBlack ease-in-out 
-                        duration-500
-                        ">
+                    max-w-full h-full bg-button px-8 py-1 rounded-lg
+                    text-base text-colorWhite hover:bg-colorBlack ease-in-out 
+                    duration-500
+                    ">
                                 Comprar
                             </button>
                             <span className="
-                            text-base text-center text-button
-                        ">
-                                R$200
+                        text-base text-center text-button
+                    ">
+                                {item.preco}
                             </span>
                         </div>
                     </div>
 
-                    <div className="
+                ))}
+
+
+                    {/* <div className="
                  w-full min-h-full p-4 flex items-center
                  justify-center flex-col border-border1 
                  rounded-lg gap-4
@@ -245,8 +259,8 @@ const SectionProduct = () => {
                         ">
                                 R$200
                             </span>
-                        </div>
-                    </div>
+                        </div> */}
+                    {/* </div> */}
                 </div>
             </div>
         </div>
